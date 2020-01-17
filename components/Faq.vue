c8b885f4f4139009c881ed2cad8a20d6a4abd25a
atjhoendZ <achunarmando@gmail.com>
2020-01-17 11:42:47 +0700

    Faq move to component add new faq

---
+++ components/Faq.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel
+      v-for="faq in faqs"
+    >
+      <v-expansion-panel-header class="body-1 font-weight-bold">
+        {{ faq.q }}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content class="body-1">
+        <b>Answer : </b>{{ faq.a }}
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    faqs: [
+      {
+        q: 'Bahasa apa yang harus digunakan untuk Cover Letter, CV, dan Self-Introduction Video?',
+        a: 'Bahasa yang digunakan bebas, Bahasa Indonesia atau Bahasa Inggris tidak masalah.'
+      },
+      {
+        q: 'Bagaimana isi Self-Introduction Video?',
+        a: 'Harus memperlihatkan diri, perkenalkan diri, alasan mau menjadi asprak, buat sekreatif mungkin. Pastikan account Instagram tidak di-private ya.'
+      },
+      {
+        q: 'Isi Cover Letter?',
+        a: 'Sesuai dengan isi cover letter formal pada umumnya, seperti saat akan melamar suatu pekerjaan.'
+      },
+      {
+        q: 'Mata kuliah apa saja yang harus minimal B?',
+        a: 'Tidak ada ketentuan mata kuliah apa saja, yang penting IPK diatas 3.00. Persyaratan nilai mata kuliah tertentu minimal B hanya digunakan ketika memilih mata kuliah apa yang diajarkan nanti.'
+      },
+      {
+        q: 'Test coding? Apa yang harus dipersiapkan?',
+        a: 'Tidak rumit, test ini untuk melihat sejauh mana kalian menyelesaikan suatu permasalahan dasar dengan logika sederhana. Untuk apa yang harus dipersiapkan, bagaimana prosesnya akan diinformasikan kepada pendaftar.'
+      },
+      {
+        q: 'Upload jawaban online assessment kemana?',
+        a: 'Upload di web ini, nanti akan muncul button upload di halaman timeline recruitment pada tanggal 2 Februari 2020 pukul 13.00 WIB.'
+      }
+    ]
+  })
+}
+</script>
--- pages/faq.vue
+++ pages/faq.vue
@@ -19,18 +19,7 @@
       md10
       class="mt-5"
     >
-      <v-expansion-panels>
-        <v-expansion-panel
-          v-for="faq in faqs"
-        >
-          <v-expansion-panel-header class="body-1 font-weight-bold">
-            {{ faq.q }}
-          </v-expansion-panel-header>
-          <v-expansion-panel-content class="body-1">
-            <b>Answer : </b>{{ faq.a }}
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+      <Faq />
     </v-flex>
     <div class="line2">
     </div>
@@ -50,36 +39,16 @@
 
 <script>
 import Banner from '~/components/Banner.vue'
+import Faq from '~/components/Faq.vue'
 
 export default {
   components: {
-    Banner
+    Banner,
+    Faq
   },
   data: () => ({
     cp1: 'Marcell Antonius (Line ID : marcell_antonius14)',
-    cp2: 'Faradilla Azranur (Line ID : fardil7)',
-    faqs: [
-      {
-        q: 'Bahasa apa yang harus digunakan untuk Cover Letter, CV, dan Self-Introduction Video?',
-        a: 'Bahasa yang digunakan bebas, Bahasa Indonesia atau Bahasa Inggris tidak masalah.'
-      },
-      {
-        q: 'Bagaimana isi Self-Introduction Video?',
-        a: 'Harus memperlihatkan diri, perkenalkan diri, alasan mau menjadi asprak, buat sekreatif mungkin. Pastikan account Instagram tidak di-private ya.'
-      },
-      {
-        q: 'Isi Cover Letter?',
-        a: 'Sesuai dengan isi cover letter formal pada umumnya, seperti saat akan melamar suatu pekerjaan.'
-      },
-      {
-        q: 'Mata kuliah apa saja yang harus minimal B?',
-        a: 'Tidak ada ketentuan mata kuliah apa saja, yang penting IPK diatas 3.00. Persyaratan nilai mata kuliah tertentu minimal B hanya digunakan ketika memilih mata kuliah apa yang diajarkan nanti.'
-      },
-      {
-        q: 'Test coding? Apa yang harus dipersiapkan?',
-        a: 'Tidak rumit, test ini untuk melihat sejauh mana kalian menyelesaikan suatu permasalahan dasar dengan logika sederhana. Untuk apa yang harus dipersiapkan, bagaimana prosesnya akan diinformasikan kepada pendaftar.'
-      }
-    ]
+    cp2: 'Faradilla Azranur (Line ID : fardil7)'
   })
 }
 </script>
